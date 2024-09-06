413a302ab7099b7fe2b71bdfd473a1c5855b6b06
Fernando Sousa Matos <est-fernando.matos@tjsp.jus.br>
2024-09-06 13:02:15 -0300

    firstcommit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+console.log(dados[2]);
+
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+
+let campoPesquisa = document.getElementById("campoPesquisa").value
+
+let resultados = ""
+
+for (let dado of dados) {
+    resultados +=`
+            <div class="item-resultado">
+                <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dados.link} target="_blank">Mais informações</a>
+            </div>`
+}
+
+section.innerHTML = resultados
+
+
+}
+
+
+
---
+++ dados.js
@@ -0,0 +1,19 @@
+ let dados= [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos de idade, já coleciona títulos e patrocinadores, sendo um dos maiores nomes do skate feminino no Brasil e no mundo.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+     },
+     {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade: A Maior Medalhista Olímpica Brasileira Rebeca Andrade é uma ginasta artística brasileira que fez história nos Jogos Olímpicos. Com seu talento e dedicação, ela se tornou a maior medalhista olímpica do Brasil, conquistando diversas medalhas em diferentes modalidades da ginástica artística. Sua trajetória inspiradora e suas conquistas a tornaram um ícone do esporte nacional, sendo admirada por milhões de brasileiros. Rebeca representa a força, a determinação e o orgulho do Brasil no cenário esportivo internacional.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+     },
+     {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é uma das grandes promessas do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+     }
+ ];
+
+ 
