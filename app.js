d4f38882e408fced3b92b9f1aa20878deff12542
Fernando Sousa Matos <est-fernando.matos@tjsp.jus.br>
2024-09-10 10:41:41 -0300

    ending

--- app.js
+++ app.js
@@ -4,17 +4,32 @@ console.log(dados[2]);
 function pesquisar() {
     let section = document.getElementById("resultados-pesquisa")
 
-let campoPesquisa = document.getElementById("campoPesquisa").value
+let campoPesquisa = document.getElementById("campo-pesquisa").value
+if(campoPesquisa ==""){
+    section.innerHTML = "<p>Nada foi encontrado</p>"
+    return
+}
+campoPesquisa =  campoPesquisa.toLowerCase()
+let resultados = "";
+let titulo = "";
+let descricao = "";
 
-let resultados = ""
+
+console.log(campoPesquisa);
 
 for (let dado of dados) {
+    titulo =  dado.titulo.toLowerCase()
+    descricao =  dado.descricao.toLowerCase()
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+        
     resultados +=`
             <div class="item-resultado">
                 <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
                 <p class="descricao-meta">${dado.descricao}</p>
                 <a href=${dados.link} target="_blank">Mais informações</a>
             </div>`
+    }
+    
 }
 
 section.innerHTML = resultados
